1413fabf61d28efb42e9a7e65645a47ee49c965f
marioanalisededadosgit <marioanalisededados@gmail.com>
2026-03-18 11:12:15 -0300

    criar cadastro de cliente e alimentar a planilha

--- backend/app.py
+++ backend/app.py
@@ -1,81 +1,3 @@
-# from flask import Flask, request, jsonify
-# from flask_cors import CORS
-# import gspread
-# from google.oauth2.service_account import Credentials
-# from dotenv import load_dotenv
-# import os
-
-# load_dotenv()
-
-# app = Flask(__name__)
-# CORS(app)
-
-# # Configuração Google Sheets
-# SCOPES = [
-#     'https://www.googleapis.com/auth/spreadsheets',
-#     'https://www.googleapis.com/auth/drive'
-# ]
-
-# def conectar_sheets():
-#     creds = Credentials.from_service_account_file('credentials.json', scopes=SCOPES)
-#     client = gspread.authorize(creds)
-#     spreadsheet = client.open_by_key(os.getenv('SPREADSHEET_ID'))
-#     return spreadsheet
-
-# # Rota para buscar produtos
-# @app.route('/produtos', methods=['GET'])
-# def get_produtos():
-#     try:
-#         spreadsheet = conectar_sheets()
-#         sheet = spreadsheet.worksheet('PRODUTO')
-#         dados = sheet.get_all_records()
-#         produtos = [
-#             {
-#                 'id': p['ID_ITEM'],
-#                 'nome': p['PRODUTO'],
-#                 'preco': str(p['PRECO_VENDA']).replace('R$', '').replace('.', '').replace(',', '.').strip()
-#             }
-#             for p in dados if p['PRODUTO']
-#         ]
-#         return jsonify({'sucesso': True, 'produtos': produtos})
-#     except Exception as e:
-#         return jsonify({'sucesso': False, 'erro': str(e)}), 500
-
-# # Rota para registrar venda
-# @app.route('/venda', methods=['POST'])
-# def registrar_venda():
-#     try:
-#         dados = request.json
-#         spreadsheet = conectar_sheets()
-#         sheet_vendas = spreadsheet.worksheet('VENDAS')
-
-#         # Calcula valores
-#         quantidade = float(dados['quantidade'])
-#         preco_unitario = float(dados['preco_unitario'])
-
-#         # Descobre a próxima linha vazia
-#         proxima_linha = len(sheet_vendas.get_all_values()) + 1
-
-#         # Escreve apenas nas colunas sem fórmula
-#         sheet_vendas.batch_update([
-#             {'range': f'C{proxima_linha}', 'values': [[dados['produto']]]},
-#             {'range': f'D{proxima_linha}', 'values': [[quantidade]]},
-#             {'range': f'F{proxima_linha}', 'values': [[preco_unitario]]},
-#             {'range': f'H{proxima_linha}', 'values': [[dados['data_venda']]]},
-#             {'range': f'I{proxima_linha}', 'values': [[dados['cliente']]]},
-#             {'range': f'J{proxima_linha}', 'values': [[dados['status_pagamento']]]},
-#             {'range': f'K{proxima_linha}', 'values': [[dados['condicao']]]}
-#         ])
-
-#         return jsonify({'sucesso': True, 'mensagem': 'Venda registrada com sucesso!'})
-#     except Exception as e:
-#         return jsonify({'sucesso': False, 'erro': str(e)}), 500
-
-# if __name__ == '__main__':
-#     app.run(debug=True, port=5000)
-
-#---------------------------------------------------------------------------------------
-
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import gspread
@@ -154,6 +76,36 @@ def registrar_venda():
         ])
 
         return jsonify({'sucesso': True, 'mensagem': 'Venda registrada com sucesso!'})
+    except Exception as e:
+        return jsonify({'sucesso': False, 'erro': str(e)}), 500
+
+# Rota para cadastrar cliente     add 18/03/2026 Mário
+@app.route('/cliente', methods=['POST'])
+def cadastrar_cliente():
+    try:
+        dados = request.json
+        spreadsheet = conectar_sheets()
+        sheet_clientes = spreadsheet.worksheet('CLIENTE')
+
+        # Encontra próxima linha vazia pela coluna A (ID_CLIENTE)
+        coluna_nome = sheet_clientes.col_values(1)
+        proxima = len([x for x in coluna_nome if x.strip() != '']) + 1
+
+        from datetime import datetime
+        data_cadastro = datetime.now().strftime('%Y-%m-%d')
+
+        sheet_clientes.batch_update([
+            {'range': f'B{proxima}', 'values': [[dados['nome_cliente']]]},
+            {'range': f'C{proxima}', 'values': [[dados['cpf']]]},
+            {'range': f'D{proxima}', 'values': [[dados['telefone']]]},
+            {'range': f'E{proxima}', 'values': [[dados['email']]]},
+            {'range': f'F{proxima}', 'values': [[dados['endereco']]]},
+            {'range': f'G{proxima}', 'values': [[data_cadastro]]},
+            {'range': f'H{proxima}', 'values': [[dados['tipo_cliente']]]},
+            {'range': f'I{proxima}', 'values': [[dados['observacao']]]}
+        ])
+
+        return jsonify({'sucesso': True, 'mensagem': 'Cliente cadastrado com sucesso!'})
     except Exception as e:
         return jsonify({'sucesso': False, 'erro': str(e)}), 500
 
